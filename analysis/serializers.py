f1251e125561ed2d0ef09c89138c2a4d0b431e35
Leonardo Aranda <leonardoaranda@gmail.com>
2015-05-07 00:45:50 -0300

    Remove result field

--- analysis/serializers.py
+++ analysis/serializers.py
@@ -3,8 +3,7 @@ from rest_framework import serializers
 
 class AnalysisSerializer(serializers.ModelSerializer):
 	output = serializers.CharField(required=False)
-	result = serializers.CharField(required=False)
 
     	class Meta:
         	model = Analysis
-        	fields = ('id', 'dataset', 'algorithm', 'schema', 'output', 'result', 'created_at')
+        	fields = ('id', 'dataset', 'algorithm', 'schema', 'output', 'created_at')
