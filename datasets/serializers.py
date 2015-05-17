bbc5fa67031dac46606aade84934aa81b1bac759
Leonardo Aranda <leonardoaranda@gmail.com>
2015-05-17 02:53:08 -0300

    Fix serializer model

--- datasets/serializers.py
+++ datasets/serializers.py
@@ -1,7 +1,7 @@
 from datasets.models import Dataset
 from rest_framework import serializers
 
-class DatasetSerializer(serializers.HyperlinkedModelSerializer):
+class DatasetSerializer(serializers.ModelSerializer):
     class Meta:
         model = Dataset
         fields = ('id', 'name', 'user', 'data', 'created_at')
